fcfea7bc7b02faaf1da1a2d9d204e307105ef72b
mori-dev <mori.dev.asdf@gmail.com>
2018-01-15 14:17:53 +0900

    chore: add basic settings for babel-plugin-module-resolver

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 // @flow
-import React, { Component } from 'react'
-import { Platform, StyleSheet, Text, View } from 'react-native'
-import ModuleRoot from './app/modules/module_root'
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import ModuleRoot from "app/modules/module_root";
 
 export default class App extends Component<{}> {
   render() {
-    return <ModuleRoot />
+    return <ModuleRoot />;
   }
 }
