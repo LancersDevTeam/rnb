b89b93cbe34f7dd05e527988c3ae499466798b84
mori-dev <mori.dev.asdf@gmail.com>
2018-01-15 14:29:02 +0900

    fix: fix lint error

--- src/modules/module_root.js
+++ src/modules/module_root.js
@@ -1,16 +1,16 @@
 // @flow
-import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import React, { Component } from 'react'
+import { Platform, StyleSheet, Text, View } from 'react-native'
 
-type State = any;
-type Props = any;
+type State = any
+type Props = any
 
 const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu'
+})
 
 export default class ModuleRoot extends Component<Props, State> {
   render() {
@@ -20,25 +20,25 @@ export default class ModuleRoot extends Component<Props, State> {
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
-    textAlign: "center",
+    textAlign: 'center',
     margin: 10
   },
   instructions: {
-    textAlign: "center",
-    color: "#333333",
+    textAlign: 'center',
+    color: '#333333',
     marginBottom: 5
   }
-});
+})
